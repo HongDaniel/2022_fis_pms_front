75c3de447c8b1f690bae1cb4e5fa70743f177349
cy <cy@cyui-MacBookAir.local>
2022-02-21 20:32:50 +0900

    feature/ImageContainer

--- src/Atom/AppendDots.js
+++ src/Atom/AppendDots.js
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "../Media/slick-theme.css";
 import "../Media/slick.css";
-
+import puppy from "../Media/images/img1.png"
+import puppy2 from "../Media/images/img2.jpeg"
+import puppy3 from "../Media/images/img3.jpeg"
 
 export default class AppendDots extends Component {
+    constructor(props) {
+        super(props);
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+    }
+    next() {
+        this.slider.slickNext();
+    }
+    previous() {
+        this.slider.slickPrev();
+    }
     render() {
         const settings = {
             dots: true,
@@ -12,12 +25,14 @@ export default class AppendDots extends Component {
             speed: 500,
             slidesToShow: 1,
             slidesToScroll: 1,
+            adaptiveHeight: true,
+            width: 100,
             appendDots: dots => (
                 <div
                     style={{
                         backgroundColor: "#ddd",
                         borderRadius: "10px",
-                        padding: "10px"
+                        padding: "10px",
                     }}
                 >
                     <ul style={{ margin: "0px" }}> {dots} </ul>
@@ -26,9 +41,12 @@ export default class AppendDots extends Component {
             customPaging: i => (
                 <div
                     style={{
-                        width: "30px",
-                        color: "blue",
-                        border: "1px blue solid"
+                        width: "50px",
+                        height: '50px',
+                        fontSize: '25pt',
+                        color: "black",
+                        border: "1px black solid",
+                        borderRadius: '10px',
                     }}
                 >
                     {i + 1}
@@ -37,26 +55,25 @@ export default class AppendDots extends Component {
         };
         return (
             <div>
-                <Slider {...settings}>
+                <Slider ref={c => (this.slider = c)} {...settings}>
                     <div>
-                        <h3>1</h3>
+                        <img style={{width: '1100px'}} alt={'image'} src={puppy} />
                     </div>
                     <div>
-                        <h3>2</h3>
+                        <img style={{width: '1100px'}} alt={'image'} src={puppy2} />
                     </div>
                     <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
+                        <img style={{width: '1100px'}} alt={'image'} src={puppy3} />
                     </div>
                 </Slider>
+                <div style={{ textAlign: "center", position: 'absolute' }}>
+                    <button className="button" onClick={this.previous}>
+                        이전
+                    </button>
+                    <button className="button" onClick={this.next}>
+                        다음
+                    </button>
+                </div>
             </div>
         );
     }
