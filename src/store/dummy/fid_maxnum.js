8851c8038bc02cd707778bc5ab54de621d625ff8
hongdaniel <jungd1223@naver.com>
2022-03-16 17:30:38 +0900

    feature/scanpage

--- src/store/dummy/fid_maxnum.js
+++ src/store/dummy/fid_maxnum.js
@@ -1,8 +1,5 @@
 export const fid_maxnum = [
-    {label:'1', maxnum:3},
-    {label:'2', maxnum:3},
-    {label:'3', maxnum:3},
-    {label:'4', maxnum:3},
-    {label:'5', maxnum:3},
-    {label:'7690', maxnum:3},
+    {label:'7689', maxnum:1},
+    {label:'7690', maxnum:1},
+    {label:'7692', maxnum:1},
 ];
